7b73be46950b953d3e067de452eda4fbc99a55ca
DiazAdriel0 <diazadriel0@gmail.com>
2023-04-26 20:06:35 -0300

    Correcciones y código mas ordenado

--- script.js
+++ script.js
@@ -1,45 +1,32 @@
-let id = 0;
+//Declaración de variables
+const nombres = [document.getElementById("nombre0").textContent,document.getElementById("nombre1").textContent,document.getElementById("nombre2").textContent,document.getElementById("nombre3").textContent,document.getElementById("nombre4").textContent,document.getElementById("nombre5").textContent,document.getElementById("nombre6").textContent,document.getElementById("nombre7").textContent];
+//Precios segun el diseño
+const precios = [2500,3200,2800,2700,5000,8200,10000,10000];
+const productos = [];
+let medidasDom = [];
+const botonesAgregar = [];
+const cantidadesDom = [];
+let productosBolsa = [];
+const rutasDeImagenes = [];
+const bolsaBoton = document.getElementById("bolsaBoton");
+let vaciarBolsa = document.getElementById("vaciarBolsa");
+let productosEnLS = [];
+let contador = productosEnLS.length;
+let contadorProductos = document.getElementById('contadorProductos');
+let span = document.createElement('span');
 class Producto{
     constructor(nombre,precio){
-        this.id = id++
         this.nombre = nombre;
         this.medidas = ["Almond","Medium Square","Medium Coffin","Long Stiletto","Long Coffin","Long Tapered Square","Extra Long Stiletto","Extra Long Tapered Square","Extra Long Coffin","Triple Extra Long Tapered Square"];
         this.precio = precio;
     }
 }
 
+//Declaración de funciones
 function crearArrayDePrecios(precio) {
     let arrayDePrecios = [precio,Math.round(precio*1.1),Math.round(precio*1.1),Math.round(precio*1.3),Math.round(precio*1.3),Math.round(precio*1.3),Math.round(precio*1.6),Math.round(precio*1.6),Math.round(precio*1.6),Math.round(precio*2)];
     return arrayDePrecios;
 }
-
-
-const nombres = [document.getElementById("nombre0").textContent,document.getElementById("nombre1").textContent,document.getElementById("nombre2").textContent,document.getElementById("nombre3").textContent,document.getElementById("nombre4").textContent,document.getElementById("nombre5").textContent,document.getElementById("nombre6").textContent,document.getElementById("nombre7").textContent];
-
-//Precios segun el diseño
-const precios = [2500,3200,2800,2700,5000,8200,10000,10000];
-
-const productos = []
-
-for (let i = 0; i < nombres.length; i++) {
-    const nombre = nombres[i];
-    let arrayDePrecios = crearArrayDePrecios(precios[i])
-    const producto = new Producto(nombre,arrayDePrecios)
-    productos.push(producto)
-}
-
-let medidasDom = [];
-for (let i = 0; i < 8; i++) {
-    medidasDom.push(document.getElementById(`medida${i}`)) 
-}
-
-for (let i = 0; i < medidasDom.length; i++) {
-    const element = medidasDom[i];
-    element.addEventListener("change", function(e) {
-        mostrarPrecio(e, productos[i].id);
-    });
-}
-
 function mostrarPrecio(e, id){
     let medidaSeleccionada = e.target.value;
     let preciosDom = []
@@ -57,41 +44,6 @@ function mostrarPrecio(e, id){
     preciosDom[id].innerHTML = ""
     preciosDom[id].appendChild(span)
 }
-
-const botonesAgregar = [];
-
-for (let i = 0; i < 8; i++) {
-    botonesAgregar.push(document.getElementById(`agregarALaBolsa${i}`));
-}
-
-const cantidadesDom = [];
-
-for (let i = 0; i < 8; i++) {
-    cantidadesDom.push(document.getElementById(`cantidad${i}`))
-}
-
-
-
-for (let i = 0; i < botonesAgregar.length; i++) {
-    const element = botonesAgregar[i];
-    element.addEventListener("click", function() {
-        agregarProducto(productos[i].id);
-    });
-}
-
-let productosBolsa = [];
-if (localStorage.length > 0) {
-    productosBolsa.push(...JSON.parse(localStorage.getItem('productosBolsa')));
-}
-
-const rutasDeImagenes = [];
-
-for (let i = 0; i < 8; i++) {
-    const imagen = document.getElementById(`img${i}`);
-    const ruta = imagen.getAttribute('src');
-    rutasDeImagenes.push(ruta)    
-}
-
 function agregarProducto(ident){
     let productoElegido = {
         nombre: productos[ident].nombre,
@@ -101,44 +53,69 @@ function agregarProducto(ident){
         ruta: rutasDeImagenes[ident],
     }
     if (productoElegido["medida"] == "") {
-        alert("Seleccione una medida") //Reemplazar por Toastify
+        swal({
+            text: "Para agregar un producto debe seleccionar el tamaño",
+        });
     }else if (productoElegido["cantidad"] == ""){
-        alert("Seleccione cantidad de productos") //Reemplazar por Toastify
+        swal({
+            text: "Para agregar un producto debe seleccionar cantidad",
+        });
     }
     else{
         productosBolsa.push(productoElegido);
-    }
-    //Recorro productosBolsa para eliminar un producto repetido y sumarle 1 a cantidad
-    for (let i = 0; i < productosBolsa.length; i++) {
-        const producto = productosBolsa[i]
-        for (let j = 0; j < productosBolsa.length; j++) {
-            const element = productosBolsa[j]
-            if (i == j){
-                continue;
-            }
-            if (producto.nombre == element.nombre && producto.medida == element.medida){
-                producto.cantidad = Number(producto.cantidad) + Number(element.cantidad);
-                productosBolsa.splice(j,1)
-                localStorage.removeItem(element)
-            }
-        } 
-    }
-    localStorage.setItem("productosBolsa", JSON.stringify(productosBolsa))
-    //Meter un Toastify de producto agregado a la bolsa
+        //Recorro productosBolsa para eliminar un producto repetido y sumarle la cantidad
+        for (let i = 0; i < productosBolsa.length; i++) {
+            const producto = productosBolsa[i]
+            for (let j = 0; j < productosBolsa.length; j++) {
+                const element = productosBolsa[j]
+                if (i == j){
+                    continue;
+                }
+                if (producto.nombre == element.nombre && producto.medida == element.medida){
+                    producto.cantidad = Number(producto.cantidad) + Number(element.cantidad);
+                    productosBolsa.splice(j,1)
+                    localStorage.removeItem(element)
+                }
+            } 
+        }
+        localStorage.setItem("productosBolsa", JSON.stringify(productosBolsa))
+        Toastify({
+            text: "Producto agregado a la bolsa",
+            duration: 1500,
+            gravity: "top",
+            position: "center",
+            offset: {
+                y: 80,
+            },
+            style: {
+            background: "linear-gradient(to right, #e600ac, #bd008e)",
+            },
+        }).showToast();
+        if(localStorage.getItem('productosBolsa')){
+            productosEnLS = [];
+            productosEnLS.push(...JSON.parse(localStorage.getItem('productosBolsa')));
+            contador = productosEnLS.length;
+            span.innerHTML = "";
+            span.innerHTML = `<span>${contador}</span>`;
+            contadorProductos.appendChild(span)
+        }
+    } 
 }
-
-
-
-const bolsaBoton = document.getElementById("bolsaBoton");
-bolsaBoton.addEventListener("click", mostrarBolsaDeCompras);
-
 function mostrarBolsaDeCompras() {
     let productosEnLocalStorage = [];
     const bolsa = document.getElementById("bolsa");
     bolsa.innerHTML = ""
     let subTotales = [];
     if(localStorage.length == 0){
-        alert("Aún no agregó productos a la bolsa");//Reemplazar con Toastify
+        Toastify({
+            text: "Aún no agregó productos a la bolsa",
+            duration: 2000,
+            gravity: "top",
+            position: "center",
+            style: {
+              background: "linear-gradient(to right, #e600ac, #bd008e)",
+            },
+        }).showToast();
     }else{
         productosEnLocalStorage.push(...JSON.parse(localStorage.getItem('productosBolsa')));
         for (let i = 0; i < productosEnLocalStorage.length; i++) {
@@ -185,7 +162,70 @@ function mostrarBolsaDeCompras() {
         bolsa.appendChild(div2)
     }
 }
+function vaciarBolsaDeCompras() {
+    localStorage.removeItem('productosBolsa')
+    productosBolsa = []
+    let div = document.getElementById('collapseWidthExample');
+    div.classList.remove('show');
+    Toastify({
+        text: "Bolsa de compras vacía",
+        duration: 1500,
+        gravity: "bottom",
+        position: "center",
+        style: {
+          background: "linear-gradient(to right, #e600ac, #bd008e)",
+        },
+    }).showToast();
+    //Borro el span que contaba productos en la bolsa
+    contadorProductos.appendChild(span)
+    contadorProductos.removeChild(span)
+}
+function crearNuevoProducto(i) {
+    const nombre = nombres[i];
+    let arrayDePrecios = crearArrayDePrecios(precios[i]);
+    const producto = new Producto(nombre,arrayDePrecios);
+    productos.push(producto);
+}
+function obtenerRutasDeImagenes(i) {
+    const imagen = document.getElementById(`img${i}`);
+    const ruta = imagen.getAttribute('src');
+    rutasDeImagenes.push(ruta)
+}
 
+//Obtengo elementos al DOM
+for (let i = 0; i < nombres.length; i++) {
+    medidasDom.push(document.getElementById(`medida${i}`)) 
+}
+for (let i = 0; i < nombres.length; i++) {
+    botonesAgregar.push(document.getElementById(`agregarALaBolsa${i}`));
+}
+for (let i = 0; i < nombres.length; i++) {
+    cantidadesDom.push(document.getElementById(`cantidad${i}`))
+}
+for (let i = 0; i < nombres.length; i++) {
+    obtenerRutasDeImagenes(i);
+}
+
+//Creo los productos
+for (let i = 0; i < nombres.length; i++) {
+    crearNuevoProducto(i)
+}
+
+//Eventos
+for (let i = 0; i < medidasDom.length; i++) {
+    const element = medidasDom[i];
+    element.addEventListener("change", function(e) {
+        mostrarPrecio(e, i);
+    });
+}
+for (let i = 0; i < botonesAgregar.length; i++) {
+    const element = botonesAgregar[i];
+    element.addEventListener("click", function() {
+        agregarProducto(i);
+    });
+}
+bolsaBoton.addEventListener("click", mostrarBolsaDeCompras);
+vaciarBolsa.addEventListener("click", vaciarBolsaDeCompras);
 //Esto es para que cuando se haga click por fuera del div de la bolsa de compras el contenido se deje de mostrar
 document.addEventListener('click', function(event) {
     let div = document.getElementById('collapseWidthExample');
@@ -195,14 +235,17 @@ document.addEventListener('click', function(event) {
     }
 });
 
-let vaciarBolsa = document.getElementById("vaciarBolsa");
-vaciarBolsa.addEventListener("click", vaciarBolsaDeCompras);
+//Si había algo en el local storage lo muestro en la bolsa
+if (localStorage.length > 0) {
+    productosBolsa.push(...JSON.parse(localStorage.getItem('productosBolsa')));
+}
 
-function vaciarBolsaDeCompras() {
-    localStorage.removeItem('productosBolsa')
-    //Este es el array de la funcion agregarProducto que estaba lleno de los productos del Storage entonces lo vacío
-    productosBolsa = []
-    let div = document.getElementById('collapseWidthExample');
-    div.classList.remove('show');
-    alert("Bolsa de compras vacía")//Reemplazar con Toastify
+//Contador de productos que hay en la bolsa
+if(localStorage.getItem('productosBolsa')){
+    productosEnLS = [];
+    productosEnLS.push(...JSON.parse(localStorage.getItem('productosBolsa')));
+    contador = productosEnLS.length;
+    span.innerHTML = "";
+    span.innerHTML = `<span>${contador}</span>`;
+    contadorProductos.appendChild(span);
 }
